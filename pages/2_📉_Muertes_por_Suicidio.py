27581592a67e9edd0ff0776aab0647fe55257f71
Alvaro <alvaro.m.a@outlook.es>
2025-03-27 11:31:35 -0300

    Add Streamlit pages for suicide mortality and territorial analysis with introductory content

---
+++ pages/2_📉_Muertes_por_Suicidio.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Muertes por suicidio",
+    page_icon="📉",
+)
+
+st.write("# Vigilancia epidemiológica de muertes por suicidio")
+
+
+st.markdown(
+    """
+    ### Bienvenido al apartado de la vigilancia epidemiológica de muertes por suicidio en la región de Antofagasta.
+    
+    ## 👷‍♂️ 
+
+    Estamos trabajando en la visualización de datos relacionados con muertes por suicidio en la región de Antofagasta.
+    Nuestro objetivo es proporcionar un análisis descriptivo de los datos y tendencias relacionadas con este tema, 
+    para la toma de decisiones informadas en salud pública.
+    
+    Esperamos tenerlo lo antes posibles, gracias por tu paciencia 😌🌟.
+
+    """
+)
---
+++ pages/3_🌎_Análisis_Territorial.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Análisis territorial",
+    page_icon="📉",
+)
+
+st.write("# Análisis territorial de lesiones autoinfligidas y muertes por suicidio 🌎")
+
+
+st.markdown(
+    """
+    ### Bienvenido al apartado del análisis territorial de lesiones autoinfligidas y muertes por suicidio.
+    
+    ## 👷‍♂️ 
+
+    Estamos trabajando en la visualización de los "Hotspots" de lesiones autoinfligidas y muertes por suicidio en la región de Antofagasta.
+    Nuestro objetivo es proporcionar un información geográfica detallada
+    que permita identificar patrones y la toma de decisiones informadas en salud pública.
+    
+    Esperamos tenerlo lo antes posibles, gracias por tu paciencia 😌🌟.
+
+    """
+)
